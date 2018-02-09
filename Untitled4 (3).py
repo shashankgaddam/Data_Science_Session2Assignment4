0b68832e5b4ca093c0a5b4e3116abb7e5dffddd7
shashankgaddam <34601798+shashankgaddam@users.noreply.github.com>
2018-02-09 22:32:10 +0530

    Add files via upload

---
+++ Untitled4 (3).py
@@ -0,0 +1,8 @@
+
+# coding: utf-8
+
+# In[24]:
+
+
+print("WE,THE PEOPLE OF INDIA,\n     having solemnly resolved to constitute India into a SOVEREIGN,!\n\t    SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC\n\t     and to secure to all its citizens")
+
